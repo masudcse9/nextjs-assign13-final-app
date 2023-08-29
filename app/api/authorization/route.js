b97d2b57142a243e3bbfa92a451e1ba963646a5c
masudcse9 github <masudcse9@gmail.com>
2023-08-29 23:07:45 +0600

    auth

--- app/api/authorization/route.js
+++ app/api/authorization/route.js
@@ -1,35 +1,10 @@
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
-import { headers } from 'next/headers'
+import { headers } from "next/headers";
 
 export async function GET(req, res) {
-
   const headersList = headers();
-  const list = headersList.get('Bearer');
+  const list = headersList.get("Bearer");
 
-  return NextResponse.json({"New Auth":list})
-
-
-
-
-
-
-  // const headersList = headers()
-  // const auth = headersList.has('Authorization')
-
-  // if(auth === true)
-  // {
-  //   headersList.has('Authorization')
-
-  //   return NextResponse.next(
-  //     {"message":"vaid header"},
-  //     {
-  //       status:200,
-  //       headers:{'Bearer':'Rana-ABC-123'}
-  //     })
-  // }
-  // else
-  // {
-  //   return NextResponse.json({"message":"Invaid header"},{status:401})
-  // }
+  return NextResponse.json({ "New Auth": list });
 }
